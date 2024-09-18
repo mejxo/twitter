ab30d1667b50737229f560a2a48e97001f65f7d7
mejxo <miklovicmaxo@gmail.com>
2024-09-18 12:19:25 +0200

    Update 1.00

---
+++ resources/views/example/example.blade.php
@@ -0,0 +1,13 @@
+<!Doc<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Author" content="Maxo">
+    <title>Example Website</title>
+</head>
+<body>
+    <p>Example Wb</p>
+</body>
+</html>
